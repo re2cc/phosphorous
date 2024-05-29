1fd8c63774e6ca167850f1a8d2b2f328e0fbe3ad
re2cc <42478705+re2cc@users.noreply.github.com>
2024-05-29 15:46:03 -0600

    Basic login view

--- frontend/composeApp/src/commonMain/kotlin/App.kt
+++ frontend/composeApp/src/commonMain/kotlin/App.kt
@@ -1,13 +1,17 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -18,18 +22,33 @@ import phosphorous.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        var usename: String by remember { mutableStateOf("") }
+        var password: String by remember { mutableStateOf("") }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = usename,
+                onValueChange = {usename = it}
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = password,
+                onValueChange = {password = it}
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            AnimatedVisibility(usename.isNotEmpty() && password.isNotEmpty()){
+                Button(onClick = {  }) {
+                    Text("Login")
+                }
             }
-            AnimatedVisibility(showContent) {
+
+            /*AnimatedVisibility(showContent) {
                 val greeting = remember { Greeting().greet() }
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
                     Text("Compose: $greeting")
                 }
-            }
+            }*/
         }
     }
 }
