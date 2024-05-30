7ae49389a8b3d5584e12e2390951a47448bc6bc2
ErickSS37 <erick.soriano.santillan@gmail.com>
2024-05-30 15:44:56 -0600

    Modelos actualizados

---
+++ backend/models.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from sqlmodel import Field, SQLModel, Relationship
+
+class User (SQLModel,table=True):
+    id: int|None = Field(default=None, primary_key=True)
+    username: str = Field(index=True)
+    password: str
+    icon: Optional[int]
+
+    messages:list ["Message"] = Relationship(back_populates="user")
+
+class Message (SQLModel,table=True):
+    id: int|None = Field(default=None, primary_key=True)
+    sender: str
+    content: str
+    order: int
+
+    user_id: int = Field(default=None, foreign_key="user.id")
+    user:User = Relationship(back_populates="messages")
+
+
+
